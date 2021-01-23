9fb57bedd2d3a5f6a5fe7bf16d5b0f62b96d10ba
orelzion <orelzion@gmail.com>
2021-01-23 21:41:02 +0200

    added share button and intent

--- app/src/main/java/com/github/orelzion/goofygiphy/FullScreenImageActivity.kt
+++ app/src/main/java/com/github/orelzion/goofygiphy/FullScreenImageActivity.kt
@@ -1,6 +1,8 @@
 package com.github.orelzion.goofygiphy
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
 import com.bumptech.glide.Glide
@@ -10,18 +12,34 @@ import com.github.orelzion.goofygiphy.model.network.Data
 class FullScreenImageActivity : AppCompatActivity() {
 
     private val imageView: ImageView by lazy { findViewById(R.id.fullImage) }
+    private val shareButton: Button by lazy { findViewById(R.id.shareBtn) }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         setContentView(R.layout.activity_full_screen_image)
 
-        val url = intent.getParcelableExtra<Data>("url")
-        url?.let {
-            loadImage(it.images.downsized_medium.url)
+        intent.getParcelableExtra<Data>("url")?.let {
+            setupViews(it.images.downsized_medium.url)
         }
     }
 
+    private fun setupViews(url: String) {
+        loadImage(url)
+
+        shareButton.setOnClickListener {
+            shareWithOtherActivities(url)
+        }
+    }
+
+    private fun shareWithOtherActivities(url: String) {
+        val sendIntent = Intent().apply {
+            action = Intent.ACTION_SEND
+            type = "text/plain"
+            putExtra(Intent.EXTRA_TEXT, "Check out this cool gif $url")
+        }
+        startActivity(sendIntent)
+    }
 
     private fun loadImage(url: String) {
         Glide.with(this)
